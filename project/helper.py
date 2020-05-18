8957b914a4a26465a4af115f8935f77ab8ca9e46
rahmatnazali95 <rahmatnazali95@gmail.com>
2020-05-18 08:58:59 +0700

    helper and unit test for Game creation

--- project/helper.py
+++ project/helper.py
@@ -12,3 +12,12 @@ def generate_platform(platform_name, has_games=None):
         "name": platform_name,
         "hasGames": has_games if has_games else []
     }
+
+def generate_game(name, developer, ofGenre=None, onPlatform=None):
+    return {
+        "uuid": generate_id(),
+        "name": name,
+        "developer": developer,
+        "ofGenre": ofGenre if ofGenre else [],
+        "onPlatform": onPlatform if onPlatform else [],
+    }
--- test/create_schema_test.py
+++ test/create_schema_test.py
@@ -21,18 +21,35 @@ class TestCreateGameSchema(unittest.TestCase):
         self.assertEqual({'Game', 'Subtitle', 'Tag', 'Video', 'Platform', 'Genre'}, set([e.get("class") for e in class_list]))
 
     def test_create_simple_platform(self):
-        platform1 = helper.generate_platform("PC", [])
+        platform1 = helper.generate_platform("Genre 1", [])
         self.client.create_thing(helper.extract_attribute(platform1), "Platform", platform1["uuid"])
 
         time.sleep(2)
 
-        output_platform_1 = self.client.get_thing(platform1["uuid"])
-        self.assertEqual(output_platform_1.get("class"), "Platform")
-        self.assertEqual(output_platform_1.get("id"), platform1["uuid"])
-        self.assertEqual(output_platform_1.get("schema").get("name"), platform1["name"])
-        self.assertEqual(output_platform_1.get("schema").get("hasGames"), platform1["hasGames"])
+        output_platform1 = self.client.get_thing(platform1["uuid"])
+        self.assertEqual(output_platform1.get("class"), "Platform")
+        self.assertEqual(output_platform1.get("id"), platform1["uuid"])
+        self.assertEqual(output_platform1.get("schema").get("name"), platform1["name"])
+        self.assertEqual(output_platform1.get("schema").get("hasGames"), platform1["hasGames"])
 
         delete_output = self.client.delete_thing(platform1["uuid"])
+        self.assertIsNone(delete_output)
+
+    def test_create_simple_game(self):
+        game1 = helper.generate_game("Game 1", "Developer 1", [], [])
+        self.client.create_thing(helper.extract_attribute(game1), "Game", game1["uuid"])
+
+        time.sleep(2)
+
+        output_game1 = self.client.get_thing(game1["uuid"])
+        self.assertEqual(output_game1.get("class"), "Game")
+        self.assertEqual(output_game1.get("id"), game1["uuid"])
+        self.assertEqual(output_game1.get("schema").get("name"), game1["name"])
+        self.assertEqual(output_game1.get("schema").get("developer"), game1["developer"])
+        self.assertEqual(output_game1.get("schema").get("ofGenre"), game1["ofGenre"])
+        self.assertEqual(output_game1.get("schema").get("onPlatform"), game1["onPlatform"])
+
+        delete_output = self.client.delete_thing(game1["uuid"])
         self.assertIsNone(delete_output)
 
 
